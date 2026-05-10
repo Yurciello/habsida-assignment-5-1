be7b4a8539092324b1b0080465f94d9620d60924
Yuriy <jurikim111@gmail.com>
2026-05-10 23:18:43 +0900

    main

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 1.265,
-  spaceBetween: 20,
-  slidesOffsetBefore: 10,
-  loop: true,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev"
-//   },
-  pagination: {
-    el: ".swiper-pagination",
+// создаём переменную для свайпера с пустым значением
+let swiper = null;
+
+// создаём функцию
+function initSwiper() {
+// условие если размер окна менее 768 пикселей
+  if (window.innerWidth < 768 && !swiper) {
+
+    // обычный скрипт для свайпера
+    swiper = new Swiper('.swiper', {
+      slidesPerView: 1.265,
+      spaceBetween: 20,
+      slidesOffsetBefore: 10,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+      }
+    });
+
+    // условие если размер окна больше или равно 768 пикселей и свайпер уже существует/создан
+  } else if (window.innerWidth >= 768 && swiper) {
+    // уничтожить свайпер
+    swiper.destroy(true, true);
+    // вернуть переменную swiper к исходному значению
+    swiper = null;
   }
-});
+}
+
+// Запуск при загрузке
+initSwiper();
+
+// Запуск при изменении размера
+window.addEventListener('resize', initSwiper);
