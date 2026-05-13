d8f2b8276285120ac48f0a66979e5894cd1ad134
Yuriy <jurikim111@gmail.com>
2026-05-14 07:18:11 +0900

    main

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+// const elements = document.querySelectorAll(".hidden-on-tablets");
+
+//     elements.forEach(element => {
+//         element.style.setProperty("display", "none", "important");
+//     });
+
+const button = document.getElementById('moreButton');
+button.addEventListener('click', () => {
+  
+  const elements = document.querySelectorAll('.hidden-on-tablets');
+    document.getElementById('moreButtonLabel').textContent = 'Показать меньше';
+    
+    elements.forEach(element => {
+      element.style.setProperty
+      ("display", "flex", "important");
+    });
+  const img = document.getElementById('moreButtonIcon');
+  img.src = "icons/less-button.svg";
+});
+
+// const button = document.getElementById("moreButton");
+// const content = document.querySelectorAll(".hidden-on-tablets");
+
+// button.addEventListener("click", () => {
+//   content.classList.toggle("hidden-on-tablets");
+
+//   if (content.classList.toggle("hidden-on-tablets")) {
+//     button.textContent = "Показать ещё";
+//   } else {
+//     button.textContent = "Скрыть";
+//   }
+// });
+
 // создаём переменную для свайпера с пустым значением
 let swiper = null;
 
@@ -31,3 +64,4 @@ initSwiper();
 
 // Запуск при изменении размера
 window.addEventListener('resize', initSwiper);
+
